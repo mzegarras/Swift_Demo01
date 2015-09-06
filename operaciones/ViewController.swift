eeb3a25cf1c32cd3550ab5cd1c805e64e1a7f6fa
manuelzegarra <mzegarra@gmail.com>
2015-09-05 23:13:40 -0500

    Versión Inicial

--- operaciones/ViewController.swift
+++ operaciones/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var segTipoOperacion: UISegmentedControl!
+    
+    
+    @IBOutlet weak var txtN1: UITextField!
+    
+    
+    
+    @IBOutlet weak var txtN2: UITextField!
+    
+    
+    
+    @IBOutlet weak var lblRp: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    @IBAction func btnCalcular(sender: UIButton) {
+        
+        
+        let n1:Int = txtN1.text.toInt()!;
+        
+        let n2:Int = txtN2.text.toInt()!;
+        
+        var rp: Double = 0;
+        
+        if segTipoOperacion.selectedSegmentIndex==0{
+            rp = Double(n1) + Double(n2);
+        }else if segTipoOperacion.selectedSegmentIndex==1{
+            rp = Double(n1) - Double(n2);
+        }else if segTipoOperacion.selectedSegmentIndex==2{
+            rp = Double(n1) * Double(n2);
+        }else if segTipoOperacion.selectedSegmentIndex==3{
+            rp = Double(n1) / Double(n2);
+        }
+        n1 + n2;
+        
+        lblRp.text = "\(rp)";
+        
+        
+        
     }
-
-
+    
 }
 
